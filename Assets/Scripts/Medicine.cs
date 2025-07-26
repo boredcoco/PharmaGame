e0771110d35927a37d8f0001fbf70953db6817f0
Faith Chua <faithchua@Faiths-MacBook-Air.local>
2025-07-26 19:40:14 +0800

    added drag and drop mechanics for medicine

--- Assets/Scripts/Medicine.cs
+++ Assets/Scripts/Medicine.cs
@@ -9,25 +9,47 @@ public class Medicine : MonoBehaviour
     public float _weight = 5f; // in mg
     public string _issueDate = "26/07/25";
 
-
     // controls view of image displayed
     ControlEnglargedView _controlEnglargedView;
+
+    // drag and drop mechanics
+    private bool dragging = false;
+    private Vector3 offset;
+    private Vector3 _beforeDragPosition;
 
     private void Start()
     {
       _controlEnglargedView = GameObject.FindWithTag(Tags.ControlEnglargedView).GetComponent<ControlEnglargedView>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-
+      if (dragging) {
+        // Move object, taking into account original offset.
+        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
+      }
     }
 
-    private void OnMouseUp()
-    {
-      _controlEnglargedView.SetMedicineView(this);
+    private void OnMouseDown() {
+      // Record the difference between the objects centre, and the clicked point on the camera plane.
+      offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+      _beforeDragPosition = transform.position;
+      dragging = true;
     }
+
+    private void OnMouseUp() {
+      if (_beforeDragPosition.Equals(transform.position))
+      {
+        _controlEnglargedView.SetMedicineView(this);
+      }
+      dragging = false;
+    }
+
+    // private void OnMouseDrag()
+    // {
+    //   offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //   dragging = true;
+    // }
 
 
 }
