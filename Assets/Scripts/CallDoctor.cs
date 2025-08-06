0928b99d5ffd39ecbb1bd5df463973117cceb6fd
Faith Chua <faithchua@Faiths-MacBook-Air.local>
2025-08-06 20:37:43 +0800

    added emergency button

---
+++ Assets/Scripts/CallDoctor.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class CallDoctor : MonoBehaviour
+{
+    public void CallDoc()
+    {
+      // insert functionality here
+      Debug.Log("Current medicine is wrong, doctor called");
+    }
+}
